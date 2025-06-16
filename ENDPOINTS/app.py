2fa9c3e0b3f14d28ed7fa4bde4c680aaa0c43a93
JoseAngelVirgenEslava <viea031210@gs.utm.mx>
2025-06-15 22:51:00 -0600

    prueba api Angel

--- ENDPOINTS/app.py
+++ ENDPOINTS/app.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "FastAPI está funcionando correctamente"}
+
+@app.get("/hola")
+def nada():
+    return {"message": "Rama de Angel"}
