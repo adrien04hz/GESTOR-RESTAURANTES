45a31628efcbfa6e57b081dc2c28ad27fff1762a
JoseAngelVirgenEslava <viea031210@gs.utm.mx>
2025-06-15 23:25:18 -0600

    algunos cambios

--- ENDPOINTS/app.py
+++ ENDPOINTS/app.py
@@ -1,11 +1,80 @@
+# main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "FastAPI está funcionando correctamente"}
+# =======================================================
+# Configuración de CORS
+# =======================================================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# =======================================================
 
-@app.get("/hola")
-def nada():
-    return {"message": "Rama de Angel"}
+
+# ======================================================
+# Conexión a la base de datos MongoDB
+# ======================================================
+MONGO_URL = os.getenv("MONGO_URL")  
+client = AsyncIOMotorClient(MONGO_URL)
+db = client["sistema_resta"]
+# ======================================================
+
+
+# ======================================================
+# colecciones de toda la base de datos
+# ======================================================
+Carrito = db["Carrito"]
+Clientes = db["Clientes"]
+Departamentos = db["Departamentos"]
+DetallesCarrito = db["DetallesCarrito"]
+Empleados = db["Empleados"]
+EmpleadosAdmin = db["EmpleadosAdmin"]
+EstadosPedido = db["EstadosPedido"]
+HorariosAdmin = db["HorariosAdmin"]
+HorariosEmpleados = db["HorariosEmpleados"]
+LogSistemaAdmin = db["LogSistemaAdmin"]
+LogSistemaEmpleados = db["LogSistemaEmpleados"]
+PayPalCliente = db["PayPalCliente"]
+PedidosCliente = db["PedidosCliente"]
+Productos = db["Productos"]
+ProductosPedido = db["ProductosPedido"]
+Roles = db["Roles"]
+RolesAdmin = db["RolesAdmin"]
+Sucursales = db["Sucursales"]
+TarjetasCredito = db["TarjetasCredito"]
+TarjetasDebito = db["TarjetasDebito"]
+# ======================================================
+
+@app.get("/")
+async def root():
+    return {"message": "Mongo conectado"}
+
+
+# endpoint para listar todos los productos de una sucursal
+@app.get("/Productos/{sucursal}")
+async def listar(sucursal: int):
+    cursor = Productos.find({"id_sucursal": sucursal}, {"_id": 0}).sort("id", 1)
+    productos = [doc async for doc in cursor]
+
+    if productos:
+        return {
+            "success": True,
+            "count": len(productos),
+            "data": productos
+        }
+    else:
+        return {
+            "success": False,
+            "message": "No se encontraron productos para la sucursal especificada."
+        }
