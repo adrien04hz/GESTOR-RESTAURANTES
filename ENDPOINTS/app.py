ded9eb031a2dff45d53fcea940d8abc533cc239e
adrien04hz <adrien.ahs@gmail.com>
2025-06-15 22:46:16 -0600

    actualizando bdd

--- ENDPOINTS/app.py
+++ ENDPOINTS/app.py
@@ -1,5 +1,6 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 import os
@@ -8,22 +9,75 @@ load_dotenv()
 
 app = FastAPI()
 
+# =======================================================
+# Configuración de CORS
+# =======================================================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# =======================================================
+
+
+# ======================================================
+# Conexión a la base de datos MongoDB
+# ======================================================
 MONGO_URL = os.getenv("MONGO_URL")  
 client = AsyncIOMotorClient(MONGO_URL)
-db = client["sample_mflix"]  # nombre de tu base
+db = client["sistema_resta"]
+# ======================================================
+
+
+# ======================================================
+# colecciones de toda la base de datos
+# ======================================================
+Carrito = db["Carrito"]
+Clientes = db["Clientes"]
+Departamentos = db["Departamentos"]
+DetallesCarrito = db["DetallesCarrito"]
+Empleados = db["Empleados"]
+EmpleadosAdmin = db["EmpleadosAdmin"]
+EstadosPedido = db["EstadosPedido"]
+HorariosAdmin = db["HorariosAdmin"]
+HorariosEmpleados = db["HorariosEmpleados"]
+LogSistemaAdmin = db["LogSistemaAdmin"]
+LogSistemaEmpleados = db["LogSistemaEmpleados"]
+PayPalCliente = db["PayPalCliente"]
+PedidosCliente = db["PedidosCliente"]
+Productos = db["Productos"]
+ProductosPedido = db["ProductosPedido"]
+Roles = db["Roles"]
+RolesAdmin = db["RolesAdmin"]
+Sucursales = db["Sucursales"]
+TarjetasCredito = db["TarjetasCredito"]
+TarjetasDebito = db["TarjetasDebito"]
+# ======================================================
 
 @app.get("/")
 async def root():
     return {"message": "Mongo conectado"}
 
-@app.get("/listar")
-async def listar():
-    documentos = []
-    cursor = db["movies"].find().limit(10)  # Limitar a 10 documentos para evitar sobrecarga
-    async for doc in cursor:
-        doc["_id"] = str(doc["_id"])
-        documentos.append(doc)
-    return documentos
+
+# endpoint para listar todos los productos de una sucursal
+@app.get("/Productos/{sucursal}")
+async def listar(sucursal: int):
+    cursor = Productos.find({"id_sucursal": sucursal}, {"_id": 0}).sort("id", 1)
+    productos = [doc async for doc in cursor]
+
+    if productos:
+        return {
+            "success": True,
+            "count": len(productos),
+            "data": productos
+        }
+    else:
+        return {
+            "success": False,
+            "message": "No se encontraron productos para la sucursal especificada."
+        }
 
 
 '''
@@ -34,3 +88,13 @@ MONGO_URL="mongodb+srv://adrienahs:4dr13nM0N0G0@gestorrestaurantes.t3vx5ir.mongo
 '''
 mongodb+srv://<db_username>:<db_password>@gestorrestaurantes.t3vx5ir.mongodb.net/?retryWrites=true&w=majority&appName=GestorRestaurantes
 '''
+
+'''
+mongoimport --uri "mongodb+srv://adrienahs:4dr13nM0N0G0@gestorrestaurantes.t3vx5ir.mongodb.net/?retryWrites=true&w=majority&appName=GestorRestaurantes" \
+            --collection "Productos" \
+            --file "/home/adrien_hs/Documentos/4_WEB_UTM/Gestor-restaurantes/ENDPOINTS/SCRAP/ShakeShack.json" \
+            --jsonArray 
+
+
+'''
+
