993c2283e62b7df17627b13fbf6c8c22d50bd813
JoseAngelVirgenEslava <viea031210@gs.utm.mx>
2025-06-19 03:21:00 -0600

    se integran el front y el backend del registro y del login

--- ENDPOINTS/Models/Cliente.py
+++ ENDPOINTS/Models/Cliente.py
@@ -1,23 +1,25 @@
 from Models.Usuario import Usuario
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, Field
 from typing import List, Optional
 
 class LoginRequest(BaseModel):
     email: EmailStr
-    contrasena: str
+    password: str
     
 class LoginResponse(BaseModel):
     nombre: str
     apellido: str
 
 class ClienteCreate(BaseModel):
-    id: int
     nombre: str
     apellido: str
     email: EmailStr
-    contrasena: str
-    metodosPago: Optional[List[str]] = []
-    id_carrito: Optional[str] = None
+    password: str = Field(min_length=6)
+    telefono: str = Field(pattern=r'^\d{10}$')
+    direccion: str
+
+    class Config:
+        extra = "forbid"
 
 class ClienteResponse(BaseModel):
     id: str
